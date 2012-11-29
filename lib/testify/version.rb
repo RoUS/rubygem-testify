872bb7813cac20bb78ca4448f55530adc2ba2f84
Ken Coar <kcoar@redhat.com>
2012-11-29 09:55:05 -0500

    Having trouble with bundling, Ruby 1.9, and rvm..

--- lib/testify/version.rb
+++ lib/testify/version.rb
@@ -1,52 +1,17 @@
 require('rubygems')
-require('time') unless (Time.now.respond_to?(:iso8601))
 require('versionomy')
 
 module Testify
 
-  @version	= Versionomy.parse('0.2.0')
-  VERSION	= @version.to_s.freeze
+  #
+  # Initial version (in Perl) which didn't get very far.
+  #
+  @version = Versionomy.parse('0.1.0')
+  #
+  # Initial port from Perl to Ruby
+  #
+  @version = @version.bump(:minor)
 
-  class << self
-
-    def version
-      return Testify.instance_variable_get(:@version)
-    end
-
-    def update_file(instr)
-      lines = File.readlines(__FILE__)
-      vpos = lines.find_index { |l| l =~ %r!^\s*VERSION\s*=! }
-      lhs = (lines[vpos-1].match(%r!^(\s*@version\s*=)!).captures[0])
-      line_new = lhs << ' @version.' << instr
-      lws = line_new.match(%r!^(\s*)!).captures[0]
-      remark = "#{lws}# Following line added #{Time.now.iso8601}\n"
-      lines.insert(vpos, remark, line_new)
-      File.open(__FILE__, 'w') { |io| io.puts(lines) }
-    end
-
-    def bump(field)
-      #
-      # Trust this to raise an exception.
-      #
-      self.version.bump(field)
-      return self.update_file("bump(#{field.to_sym.inspect})")
-    end
-
-    def method_missing(mname, *args)
-      if (self.version.respond_to?(mname))
-        return self.version.send(mname, *args)
-      end
-      mname_s = mname.to_s
-      if (mname[-1,1] == '=')
-        base_mname = mname_s.sub(%r![^_[:alnum:]]*$!, '').to_sym
-        if (self.version.respond_to?(base_mname))
-          mod = "change(#{base_mname.inspect} => #{args[0].inspect})"
-          return self.update_file(mod)
-        end
-      end
-      super
-    end
-
-  end
+  VERSION = @version.to_s
 
 end
--- testify.gemspec
+++ testify.gemspec
@@ -1,7 +1,8 @@
 # -*- encoding: utf-8 -*-
+require('rubygems')
 $:.push(File.expand_path('../lib', __FILE__))
 
-require('lib/version')
+require('testify/version')
 
 Gem::Specification.new do |s|
   s.name		= 'testify'
@@ -9,7 +10,7 @@ Gem::Specification.new do |s|
   s.platform		= Gem::Platform::RUBY
 
   s.authors		= [ 'Ken Coar' ]
-  s.summary		= %q{Interface to the Testify graphing tool}
+  s.summary		= %q{Regression test management tool}
   s.email		= %q{coar@apache.org}
   s.homepage		= %q{http://github.com/RoUS/testify}
   s.description 	= %q{}
@@ -37,6 +38,22 @@ Gem::Specification.new do |s|
   s.post_install_message = <<-EOT
   EOT
 
+  s.add_dependency('bundler', '>= 1.0.7')
+  s.add_dependency('cri')
+  s.add_dependency('gettext')
+  s.add_dependency('git')
+  s.add_dependency('highline')
+  s.add_dependency('json')
+  s.add_dependency('locale')
+  s.add_dependency('parseconfig')
+  s.add_dependency('readline')
+  s.add_dependency('versionomy')
+
+  s.add_development_dependency('aruba')
+  s.add_development_dependency('cucumber')
+  s.add_development_dependency('mocha')
   s.add_development_dependency('rake')
   s.add_development_dependency('rdoc', '>= 2.4.2')
+  s.add_development_dependency('test-unit')
+  s.add_development_dependency('yard')
 end
